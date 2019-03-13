705c38e89761de641c7db15ba45026a471488f1a
Benjamin Wasty <benny.wasty@gmail.com>
2019-03-13 08:44:39 +0100

    start stubbing gl methods required for triangle

--- yage-gl/src/gl_native.rs
+++ yage-gl/src/gl_native.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use glenum::*;
 
 #[derive(Default)]
@@ -22,5 +24,46 @@ impl GL {
         unsafe {
             gl::Clear(bit as _);
         }
+    }
+
+    pub fn create_shader(_kind: glenum::ShaderKind) -> Shader {
+        unimplemented!()
+    }
+
+    pub fn shader_source(&self, _shader: &Shader, _source: &str) {
+        unimplemented!()
+    }
+
+    // TODO!!:
+    // pub fn compile_shader()
+    // pub fn create_program()
+    // pub fn attach_shader()
+    // pub fn link_shader()
+    // pub fn use_program()
+
+    // pub fn gen_buffers()
+    // pub fn bind_buffer()
+    // pub fn buffer_data()
+    // pub fn gen_vertex_arrays()
+    // pub fn bind_vertex_array()
+
+    // pub fn get_attrib_location()
+    // pub fn vertex_attrib_pointer()
+    // pub fn enable_vertex_attrib_array()
+
+    // pub fn draw_arrays()
+}
+
+type Reference = u32;
+
+// TODO!!: OpenGL returns an int, WebGL an opaque type... trait + generics?
+// -> https://docs.rs/web-sys/0.3.15/web_sys/struct.WebGl2RenderingContext.html#method.create_shader
+#[derive(Debug)]
+/// an OpenGL shader created with [`GLContext::create_shader`]
+pub struct Shader(pub Reference);
+impl Deref for Shader {
+    type Target = Reference;
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
