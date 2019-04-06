7895613b065843e4edc1b06d423b42237b2de249
Stefan Buschmann <sbusch@sbusch.net>
2019-04-06 13:27:46 +0200

    Add alias for yage_glutin

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub extern crate yage_core as core;
 pub extern crate yage_gl as gl;
 #[cfg(not(target_arch = "wasm32"))]
-pub extern crate yage_glutin;
+pub extern crate yage_glutin as glutin;
 #[cfg(target_arch = "wasm32")]
 pub extern crate yage_web as web;
 #[cfg(feature = "gltf")]
